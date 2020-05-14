b0c52c8a37d89a433c2c310d195843a852f95293
ojasrahatebs <ojas@browserstack.com>
2020-05-14 14:55:14 +0530

    new directory

---
+++ string_manipulation/reverse_order_maintain_format.rb
@@ -0,0 +1,22 @@
+str = "I am boy"
+str=str.downcase
+z = str.split
+len_arr = Array.new()
+
+z.each do |y|
+  len_arr.append(y.length)
+end
+
+z=z.join.reverse
+z=z.chars()
+
+y=0
+for i in len_arr
+  y=y+i
+  puts "Y=#{y}"
+  z.insert(y, " ")
+  y = y+1
+end
+
+z=z.join
+print z
